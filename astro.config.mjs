d140acb85829ef2941d603a8123f729cfd653c83
Ryan Johnston <ryanjohnston@gmail.com>
2024-10-25 11:54:40 -0500

    unit 6, island time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://kaleidoscopic-fairy-8335ea.netlify.app"
+  site: "https://kaleidoscopic-fairy-8335ea.netlify.app",
+  integrations: [preact()]
 });
